c79c1349587f409001a9bcc529ccdf885a780373
TinyUniverseDevelopment <humanc0ntact0@gmail.com>
2018-08-01 16:17:59 -0400

    git push origin master

--- stack-guide-front/src/App.vue
+++ stack-guide-front/src/App.vue
@@ -40,9 +40,59 @@
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <div class="text-xs-center">
+        <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="200"
+        :nudge-left="300"
+        :nudge-bottom="15"
+        offset-x
+        absolute
+
+        >
+      <v-btn slot="activator" outline color="indigo">SignUp</v-btn>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        
+    <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="10"
+      label="Name"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Item"
+      required
+    ></v-select>
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      :disabled="!valid"
+      @click="submit"
+    >
+      submit
+    </v-btn>
+    <v-btn @click="clear">clear</v-btn>
+  </v-form>
+        </v-menu>
+
+      </div>
+      <v-btn outline color="pink accent-2">Login</v-btn>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -75,18 +125,31 @@ export default {
   name: 'App',
   data () {
     return {
+      fav: true,
+      menu:false,
+      message:false,
+      hints:true,
       clipped: false,
-      drawer: true,
+      drawer: false,
       fixed: false,
       items: [{
         icon: 'bubble_chart',
         title: 'Inspire'
       }],
-      miniVariant: false,
+      miniVariant: true,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Suppliment Stack Creater'
     }
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+.v-form{
+  padding: 30px;
+
+
+}
+</style>
+
--- stack-guide-front/src/main.js
+++ stack-guide-front/src/main.js
@@ -1,11 +1,13 @@
-import '@babel/polyfill'
+import "@babel/polyfill";
 import Vue from "vue";
-import './plugins/vuetify'
+import Vuelidate from "vuelidate";
+import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 
 new Vue({
   router,
